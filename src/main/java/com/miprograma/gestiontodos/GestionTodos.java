e2243f1c7354cdbd2617dee3359beca8219e87c9
Joshueespi60 <espinaladony95@gmail.com>
2025-04-03 16:10:22 -0500

    Primer commit

---
+++ src/main/java/com/miprograma/gestiontodos/GestionTodos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.miprograma.gestiontodos;
+
+/**
+ *
+ * @author Joshue
+ */
+public class GestionTodos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
